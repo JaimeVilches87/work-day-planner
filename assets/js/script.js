a5693514c2565c25c501033b5069e38f6d05e39f
JaimeVilches87 <jaimevilches87@gmail.com>
2021-05-22 16:33:34 -0400

    added css styling and containers to index

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,26 +9,138 @@ $(document).ready(function () {
 
 });
 
-var input_textarea1 = document.querySelector('#note1');
-var save_button1 = document.querySelector('#saveNote1');
-input_textarea1.value = localStorage.getItem('content1');
+// var input_textarea1 = $('#note1');
+// var save_button1 = $('#saveNote1');
+// input_textarea1.value = localStorage.getItem('content1');
 
-//when the Submit button is clicked, store the input of the textarea
-saveNote1.addEventListener('click', updateOutput1);
+// saveNote1.addEventListener('click', updateOutput1);
 
-//sets the input of text area into local storage
-function updateOutput1() {
-    localStorage.setItem('content1', input_textarea1.value);
-};
+// function updateOutput1() {
+//     localStorage.setItem('content1', input_textarea1.value);
+// };
+
+$(".saveBtn").on("click", function () {
+
+    var time_block = $(this).siblings(".time-block").attr("id")
+     console.log(time_block)
+    var description = $(this).siblings(".description").val()
+    console.log(description)
+    localStorage.setItem(time_block, description);
 
 
+})
 
-var input_textarea2 = document.querySelector('#note2');
-var save_button2 = document.querySelector('#saveNote2');
-input_textarea2.value = localStorage.getItem('content2');
+$(".description").each(function() {
+    
+    $(".description").val(localStorage.getItem(".time_block"))
+    
+    // $("#note1").val(localStorage.getItem("9AM"))
+})
 
-saveNote2.addEventListener('click', updateOutput2);
+var now= new Date().getHours();
+var test = $(".time-block").attr("id");
 
-function updateOutput2() {
-	localStorage.setItem('content2', input_textarea2.value);
-};
+console.log(test)
+
+if (now > test) {
+    $(".description").addClass("past");
+	}	
+    else if (now >= test && now < test++) {
+        $(".description").addClass("present");
+	}
+    else if (now < test) {
+        $(".description").addClass("future");
+	}
+
+    
+//    var now= new Date().getHours();
+
+// if (now > 9) {
+//     $("#note1").addClass("past");
+// 	}	
+//     else if (now >= 9 && now < 10) {
+//         $("#note1").addClass("present");
+// 	}
+//     else if (now < 9) {
+//         $("#note1").addClass("future");
+// 	}
+
+// if (now > 10) {
+//     $("#note2").addClass("past");
+// 	}
+//     else if (now >= 10 && now < 11) {
+//         $("#note2").addClass("present");
+// 	}
+//     else if (now < 10) {
+//         $("#note2").addClass("future");
+// 	}
+
+// if (now > 11) {
+//     $("#note3").addClass("past");
+// 	}
+//     else if (now >= 11 && now < 12) {
+//         $("#note3").addClass("present");
+// 	}
+//     else if (now < 11) {
+//         $("#note3").addClass("future");
+// 	}
+
+// if (now > 12) {
+//     $("#note4").addClass("past");
+// 	}
+//     else if (now >= 12 && now < 13) {
+//         $("#note4").addClass("present");
+// 	}
+//     else if (now < 12) {
+//         $("#note4").addClass("future");
+// 	}
+
+// if (now > 13) {
+//     $("#note5").addClass("past");
+//     }
+//     else if (now >= 13 && now < 14) {
+//         $("#note5").addClass("present");
+//     }
+//     else if (now < 13) {
+//         $("#note5").addClass("future");
+//     }
+
+// if (now > 14) {
+//     $("#note6").addClass("past");
+//     }
+//     else if (now >= 14 && now < 15) {
+//         $("#note6").addClass("present");
+//     }
+//     else if (now < 14) {
+//         $("#note6").addClass("future");
+//     }
+
+// if (now > 15) {
+//     $("#note7").addClass("past");
+// 	}
+//     else if (now >= 15 && now < 16) {
+//         $("#note7").addClass("present");
+// 	}
+//     else if (now < 15) {
+//         $("#note7").addClass("future");
+// 	}
+
+// if (now > 16) {
+//     $("#note8").addClass("past");
+// 	}
+//     else if (now >= 16 && now < 17) {
+//         $("#note8").addClass("present");
+// 	}
+//     else if (now < 16) {
+//         $("#note8").addClass("future");
+// 	}
+
+// if (now > 17) {
+//     $("#note9").addClass("past");
+// 	}
+//     else if (now >= 17 && now < 18) {
+//         $("#note9").addClass("present");
+// 	}
+//     else if (now < 17) {
+//         $("#note9").addClass("future");
+// 	}
